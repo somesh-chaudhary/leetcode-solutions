f64eb01fa78c8c3bcb24bffa978d2f22392ff1c6
Somesh Chaudhary <somesh.cx@gmail.com>
2026-09-02 14:10:45 +0530

    Time: 7 ms (79.39%), Space: 46.7 MB (69%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.java
+++ 2716-minimize-string-length/2716-minimize-string-length.java
@@ -1,12 +1,15 @@
 class Solution {
-    public int minimizedStringLength(String s){
+    public int minimizedStringLength(String s) {
+        int count =0;
 
         Set <Character> set=new HashSet<>();
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
+            if(set.contains(c)) continue;
             set.add(c);
+            count++;
         }
-        return set.size();
+        return count;
         
     }
 }
