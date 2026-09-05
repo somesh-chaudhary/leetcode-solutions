73cbc6dbd63002f6e7db677576fb84f904ba6234
Somesh Chaudhary <somesh.cx@gmail.com>
2026-09-05 23:12:19 +0530

    Time: 2 ms (99.36%), Space: 47.2 MB (40.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,9 +3,10 @@ class Solution {
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             int need=target-nums[i];
-            if(map.containsKey(need)) return new int[]{map.get(need),i};
+            if(map.containsKey(need)) return new int []{map.get(need),i};
             map.put(nums[i],i);
         }
         return new int[]{};
+        
     }
 }
